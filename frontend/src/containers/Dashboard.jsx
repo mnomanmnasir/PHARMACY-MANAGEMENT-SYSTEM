4493916b063a6921217b81624e7bfe78404c0bdc
Noman Muhammad Nasir <102538287+mnomanmnasir@users.noreply.github.com>
2024-04-24 00:22:22 +0500

    updated

--- frontend/src/containers/Dashboard.jsx
+++ frontend/src/containers/Dashboard.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Sidebar from '../components/Sidebar'
 import Cards from './Cards'
 import Sales from './Sales'
 // import Sidebar from ''
 
 const Dashboard = () => {
+    const [toggle, setToggle] = useState([])
+const Toggle = () => {
+    setToggle(!toggle)
+}
     return (
         <>
-            <Sidebar />
-            <div className='px-3'>
-                <Cards />
+            <div className="container-fluid">
+                <div className="row">
+                    {toggle && <div className="col-2 vh-100">
+                        <Sidebar />
+                    </div>}
+                    <div className='col'>
+                        {/* <Navbar /> */}
+                        <Cards Toggle={Toggle} />
+                        {/* <Sales Toggle={Toggle}/> */}
+                    </div>
+                </div>
             </div>
         </>
     )
