b95876d4e6d821469c4cbc1a9d8d2bdcd7821bcf
Noman Muhammad Nasir <102538287+mnomanmnasir@users.noreply.github.com>
2024-05-16 03:14:37 -0700

    working on purchase model

---
+++ backend/models/purchase.model.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const purchaseSchema = new mongoose.Schema({
+  productName: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  supplier: {
+    type: String,
+    required: true,
+  },
+  costPrice: {
+    type: Number,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  expireDate: {
+    type: Date,
+    required: true,
+    set: function (value) {
+      // Parse the incoming date string and convert it to a Date object
+      return new Date(value);
+    },
+  },
+  action: {
+    type: String,
+    // required: true
+  },
+});
+
+const Purchase = mongoose.model("Purchase", purchaseSchema);
+
+module.exports = Purchase;
